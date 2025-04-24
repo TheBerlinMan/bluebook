54fa8cf09b3076712255b74cac12a2ab34e17815
Berlin <tommyonik@gmail.com>
2025-04-24 15:30:13 -0400

    draft sign up form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,168 @@
 import Image from "next/image";
-
+import { Card } from "@/components/ui/card";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Info } from "lucide-react";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div className="p-6">
+      <div className="flex gap-4">
+        {/* <div className="mx-auto">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/cover.jpg"
+              alt="Blue Book"
+              width={300}
+              height={300}
+              className="rounded-md shadow-md"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        </div> */}
+        <div className="mx-auto max-w-lg">
+          <Card className="px-4 mx-auto items-center">
+            <p className="text-blue-800 font-normal">
+              Every Blue Book is unique and hand made by me.
+            </p>
+            <p className="text-blue-800 font-normal">
+              Want one? Just sign up for the waitlist.
+            </p>
+            <button className="bg-blue-800 text-white px-4 py-2 rounded-md">
+              Sign up
+            </button>
+          </Card>
+          <Card className="px-4">
+            <p className="text-blue-800 font-normal">
+              Blue Book was inspired by Moleskin pocket notebooks and the Blue
+              Book Examination booklets from my childhood. Each one is hand made
+              by me. If you'd like one, they're free. Just sign up for one on
+              the waitlist.
+            </p>
+          </Card>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
+      <div id="modal-form">
+        <form className="w-[350px] flex flex-col gap-4">
+          {/* HEADER */}
+          <div>
+            <p className="text-blue-800 font-normal">
+              {/* move into the modal */}
+              The blue book is free of charge, if you'd like one please submit
+              your name and mailing address.
+            </p>
+          </div>
+
+          {/* FIRST & LAST */}
+          <div className="flex gap-4">
+            <div className="flex flex-col">
+              <label htmlFor="name">First Name</label>
+              <input
+                type="text"
+                placeholder="First Name"
+                className="border-2 border-gray-300 rounded-md p-2"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="name">Last Name</label>
+              <input
+                type="text"
+                placeholder="Last Name"
+                className="border-2 border-gray-300 rounded-md p-2"
+              />
+            </div>
+          </div>
+
+          {/* US SHIPPING? */}
+          <div className="flex justify-between">
+            <div className="flex items-center gap-2">
+              <input type="checkbox" id="us-shipping" />
+              <label htmlFor="us-shipping">Shipping outside of the United States?</label>
+            </div>
+          </div>
+
+          {/* MAILING ADDRESS */}
+          <div>
+            <div className="flex flex-col">
+              <label htmlFor="name">Street Address</label>
+              <input
+                type="text"
+                placeholder="Street Address"
+                className="border-2 border-gray-300 rounded-md p-2"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="name">Street Address 2</label>
+              <input
+                type="text"
+                placeholder="Apt, Studio, etc."
+                className="border-2 border-gray-300 rounded-md p-2"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="name">City</label>
+              <input
+                type="text"
+                placeholder="City"
+                className="border-2 border-gray-300 rounded-md p-2"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="name">State</label>
+              <input
+                type="text"
+                placeholder="State"
+                className="border-2 border-gray-300 rounded-md p-2"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="name">Zip Code</label>
+              <input
+                type="text"
+                placeholder="Zip Code"
+                className="border-2 border-gray-300 rounded-md p-2"
+              />
+            </div>
+          </div>
+
+          {/* COVER SHIPPING */}
+          <div className="flex justify-between">
+            <div className="flex items-center gap-2">
+              <input type="checkbox" id="cover-shipping" />
+              <label htmlFor="name">I'll pay for shipping</label>
+            </div>
+            <Tooltip>
+              <TooltipTrigger>
+                <Info size={16} />
+              </TooltipTrigger>
+              <TooltipContent className="max-w-[200px]">
+                <p>
+                  You don't have to but if you'd like to help me out, that would
+                  be great!
+                </p>
+              </TooltipContent>
+            </Tooltip>
+          </div>
+
+          {/* COMMENTS */}
+          <div className="flex flex-col">
+            <label htmlFor="comments">Leave me a note...</label>
+            <textarea
+              id="comments"
+              placeholder="Message"
+              className="border-2 border-gray-300 rounded-md p-2"
+            />
+          </div>
+
+          {/* SUBMIT */}
+          <button
+            type="submit"
+            className="bg-blue-800 text-white px-4 py-2 rounded-md"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Link from "next/link";
+const Header = () => {
+  return (
+    <div style={{ backgroundColor: "#A4DCF9" }}
+        className="font-bold text-blue-800 py-4 px-6 flex justify-between items-center drop-shadow-sm shadow-blue-800"
+    >
+      <div className="text-2xl">Blue Book</div>
+      <div className="flex gap-2">
+        {/* <Link href="/signup">Sign Up</Link> */}
+        <Link href="/waitlist">Waitlist</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
