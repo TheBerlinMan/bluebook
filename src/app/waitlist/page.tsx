4cde0cf513ef50dc3a53ec052ac64c405fed2455
Berlin <tommyonik@gmail.com>
2025-05-01 11:18:08 -0400

    add customerlist comp to waitlist page

--- src/app/waitlist/page.tsx
+++ src/app/waitlist/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import CustomerList from '@/components/CustomerList'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className="p-4 mx-auto max-w-sm">
+      <CustomerList />
+    </div>  
   )
 }
 
