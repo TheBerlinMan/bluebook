74a51eec7a3bebef4748ba0ece03018c02a0359b
Berlin <tommyonik@gmail.com>
2025-05-03 19:38:41 -0400

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import { getCurrentDate } from '@/lib/utils'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="flex justify-center items-center py-4 text-blue-800 text-sm"
+      style={{ backgroundColor: 'var(--blue-book-blue)' }}
+    >
+      <p>Website by TNMA</p>
+    </div>
   )
 }
 
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getCurrentDate(): string {
+  const date = new Date();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const year = date.getFullYear();
+  return `${month}/${day}/${year}`;
+}
+
